1a6ca6aa140fb5285a9117cf7cf0fd6b52706113
Jason Johnson <jasojohnson@microsoft.com>
2024-08-29 09:19:20 +0000

    get code functioning a bit better

--- search/search.py
+++ search/search.py
@@ -1,11 +1,17 @@
 import argparse
 from search.dynamed import Dynamed
 from search.semantic_scholar import SemanticScholar
+import json
+
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 def main():
     parser = argparse.ArgumentParser(description='Script to perform medical search.')
     parser.add_argument('query', help='The search query')
-    parser.add_argument('-l', '--limit', type=int, default=10, help='Limit the number of search results (default: 10)')
+    parser.add_argument('-l', '--limit', type=int, default=100, help='Limit the number of search results (default: 100)')
     parser.add_argument('-s', '--sort', choices=['relevance', 'date'], default='relevance', help='Sort the search results by relevance or date (default: relevance)')
     parser.add_argument('-f', '--filter', choices=['all', 'articles', 'videos'], default='all', help='Filter the search results by all, articles, or videos (default: all)')
     
@@ -22,9 +28,17 @@ def main():
     data = dynamed.search(args.query, args.limit, args.sort, args.filter)
     print(data)
 
+    print('----------------------------------------')
+
     semantic_scholar = SemanticScholar()
     data = semantic_scholar.search(args.query, args.limit)
-    print(data)
+    
+    if data.get('code', '0') == '429':
+        print(f'Failed: {data['message']}')
+    else:
+        print(json.dumps(data.get('data', []), indent=2))
+        with open('output.json', 'w') as f:
+            json.dump(data.get('data', []), f)
 
 if __name__ == '__main__':
     main()
--- search/search/semantic_scholar.py
+++ search/search/semantic_scholar.py
@@ -1,16 +1,21 @@
 import requests
-
+import urllib
 
 class SemanticScholar:
     def __init__(self):
-        self.base_url = "https://api.semanticscholar.org/graph/v1/paper/autocomplete"
+        self.base_url = "https://api.semanticscholar.org/graph/v1/paper/search"
 
-    def search(self, query, limit=10):
+    def search(self, query, limit=100):
         params = {
             'query': query,
+            'fields': 'title,abstract,publicationDate,authors,influentialCitationCount,openAccessPdf,tldr,citationCount,publicationTypes,fieldsOfStudy,s2FieldsOfStudy',
+            'year': '2014-2024',
+            'limit': limit
         }
 
-        response = requests.get(self.base_url, params=params)
+        param_str = urllib.parse.urlencode(params, safe=',')
+
+        response = requests.get(self.base_url, params=param_str)
         data = response.json()
 
         return data
