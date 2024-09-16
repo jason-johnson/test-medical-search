ca276005ea1dfcf2df68d7e343e721c1377a3bc4
Jason Johnson <jasojohnson@microsoft.com>
2024-09-16 18:22:29 +0200

    initial pubmed implementation

---
+++ search/search/pubmed.py
@@ -0,0 +1,41 @@
+from .results import Redo, Success
+
+
+class Dynamed:
+    def __init__(self):
+        self.base_url = "https://???"
+        self.headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Bearer TOKEN"
+        }
+
+    async def search(self, session, searchkey, token={}):
+        params = {
+            'query': searchkey,
+            'fields': ['title'],
+        }
+
+        async with session.get(self.base_url, json=params, headers=self.headers) as resp:
+            response = await resp.json()
+            if response.get('name', '') == 'Unauthorized':
+                return Redo(searchkey, self, token)
+            
+            result = []
+            
+            for data in response.get('data', []):
+                result.append(Success(
+                    searchkey=searchkey,
+                    published_year=data.get('publicationDate', ''),
+                    published_date=data.get('year', ''),
+                    authors=[author.get('name', '') for author in data.get('authors', [])],
+                    keywords=[],
+                    title=data.get('title', ''),
+                    abstract=data.get('abstract', 'NA'),
+                    introduction='NA',
+                    results='NA',
+                    conclusion='NA',
+                    figures=[]
+                ))
+
+            return result
