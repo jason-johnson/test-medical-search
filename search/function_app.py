34d0a330913df0363e9cf17db28f7db2063f4425
Jason Johnson <jasojohnson@microsoft.com>
2024-09-25 18:38:18 +0200

    make function call search.py

--- search/function_app.py
+++ search/function_app.py
@@ -1,27 +1,28 @@
 import azure.functions as func
-import datetime
-import json
 import logging
+from search import search
 
 app = func.FunctionApp()
 
-@app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
-def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="Search", auth_level=func.AuthLevel.ANONYMOUS)
+async def Search(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    keywords = req.params.get('keywords')
+    if not keywords:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            keywords = req_body.get('keywords')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if keywords:
+        keywords = keywords.split(',')
+        results = await search(keywords, 10, 3)
+        return func.HttpResponse(f"Got: '{keywords} with {len(results)} results'. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+             "This HTTP triggered function executed successfully, but recieved no keywords",
+             status_code=400
         )
