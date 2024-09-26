fb8b6906618314a1d4bee40fbba1209ed85755ca
Jason Johnson <jasojohnson@microsoft.com>
2024-09-26 18:47:04 +0200

    add mongo writing to python app

--- app/function_app.py
+++ app/function_app.py
@@ -1,8 +1,43 @@
+import asyncio
+import os
 import azure.functions as func
 import logging
 from app import search
+import pymongo
+from itertools import islice
 
 app = func.FunctionApp()
+
+def batched(iterable, chunk_size):
+    iterator = iter(iterable)
+    while chunk := tuple(islice(iterator, chunk_size)):
+        yield chunk
+
+async def save_to_db(results):
+    CONNECTION_STRING = os.environ.get("COSMOS_CONNECTION_STRING")
+    DATATBASE_NAME = os.environ.get("COSMOS_DATABASE_NAME")
+    COLLECTION_NAME = os.environ.get("COSMOS_COLLECTION_NAME")
+    BATCH_SIZE = os.environ.get("COSMOS_BATCH_SIZE", 1000)
+
+    batch_size = 1000
+    try:
+        batch_size = int(BATCH_SIZE)
+        logging.info(f"Batch size: {batch_size}")
+    except ValueError:
+        logging.error(f"Invalid batch size: '{BATCH_SIZE}'")
+
+    results_chunks = batched(results, batch_size)
+
+    client = pymongo.MongoClient(CONNECTION_STRING)
+    database = client.get_database(DATATBASE_NAME)
+    collection = database.get_collection(COLLECTION_NAME)
+
+    for chunk in results_chunks:
+        collection.insert_many(chunk)
+        logging.info(f"Wrote {len(chunk)} results to the database")
+        await asyncio.sleep(1)
+
+    client.close()
 
 @app.route(route="Search", auth_level=func.AuthLevel.ANONYMOUS)
 async def Search(req: func.HttpRequest) -> func.HttpResponse:
@@ -20,6 +55,7 @@ async def Search(req: func.HttpRequest) -> func.HttpResponse:
     if keywords:
         keywords = keywords.split(',')
         results = await search(keywords, 10, 3)
+        await save_to_db(results)        
         return func.HttpResponse(f"Got: '{keywords} with {len(results)} results'. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
