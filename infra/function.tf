b0bfaa6338bfaf23af0d8d29efee4a6550a7c727
Jason Johnson <jasojohnson@microsoft.com>
2024-10-11 18:22:07 +0200

    fix env settings

--- infra/function.tf
+++ infra/function.tf
@@ -47,7 +47,7 @@ resource "azurerm_linux_function_app" "main" {
     container_registry_use_managed_identity = true
     application_stack {
       docker {
-        registry_url = "https://index.docker.io/v1"
+        registry_url = "https://index.docker.io"
         image_name   = "jason0077/medical-search"
         image_tag    = "latest"
       }
@@ -64,7 +64,7 @@ resource "azurerm_linux_function_app" "main" {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
     "COSMOS_CONNECTION_STRING"            = azurerm_cosmosdb_account.main.primary_mongodb_connection_string
     "COSMOS_DATABASE_NAME"                = azurerm_cosmosdb_mongo_database.main.name
-    "COSMOS_CONTAINER_NAME"               = azurerm_cosmosdb_mongo_collection.main.name
+    "COSMOS_COLLECTION_NAME"              = azurerm_cosmosdb_mongo_collection.main.name
   }
 
   identity {
