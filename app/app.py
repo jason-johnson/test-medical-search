749702602fe5d67276d9b7da80c5160642e88944
Jason Johnson <jasojohnson@microsoft.com>
2024-10-25 09:47:07 +0200

    fix bug in app logging code

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import argparse
 from functools import reduce
 import json
 import sys
+from ai.processor import PDFProcessor
 from searchlib.dynamed import Dynamed
 from searchlib.pubmed import PubMed
 from searchlib.semantic_scholar import SemanticScholar
@@ -37,6 +38,16 @@ def process_results(results, success=[]):
         success.extend(data)
     return redo, success, len(successes) > 0
 
+def result_len(results):
+    result = 0
+    for r in results:
+        if isinstance(r, list):
+            result += len(r)
+        else:
+            result += 1
+
+    return result
+
 async def search(search_keywords, concurrent_pm, concurrent_ss, concurrent_dm, retries):
     pm_conn = aiohttp.TCPConnector(limit=concurrent_pm)
     ss_conn = aiohttp.TCPConnector(limit=concurrent_ss)
@@ -50,7 +61,7 @@ async def search(search_keywords, concurrent_pm, concurrent_ss, concurrent_dm, r
 #        aiohttp.ClientSession(connector=dm_conn, timeout=timeout) as dm_session,
     ):
         results = await asyncio.gather(*(query(client, searchkeyword) for client in [SemanticScholar(ss_session), PubMed(pm_session)] for searchkeyword in search_keywords))
-        logging.info(f"Finalized initial run. Return is a list of {len(results)} outputs (total elements: {reduce(lambda count, l: count + len(l), results, 0)}).")
+        logging.info(f"Finalized initial run. Return is a list of {len(results)} outputs (total elements: {result_len(results)}.")
 
         redo, success, _ = process_results(results)
         logging.info(f"Initial run - Success: {len(success)}, Redos: {len(redo)}")
@@ -66,6 +77,20 @@ async def search(search_keywords, concurrent_pm, concurrent_ss, concurrent_dm, r
 
         return success
 
+async def process_ai(processor, doc):
+    url = doc["pdf_url"]
+    processed_data = processor.process_pdf(url, ["introduction", "results", "conclusion"])
+    new_values = { 
+        "markdown_sections": processed_data["markdown_sections"],
+        "introduction": processed_data["introduction"],
+        "results": processed_data["results"],
+        "conclusion": processed_data["conclusion"],
+        "figures": processed_data["images"],
+        "tables": processed_data["tables"],
+        "status": {"analysis": "", "ai_processing": processed_data["ai_processing"]}
+    }
+    result = {**doc, **new_values}
+    return result
 
 async def main():
     parser = argparse.ArgumentParser(description='Script to perform medical search.')
@@ -75,6 +100,7 @@ async def main():
     parser.add_argument("--concurrent-pm", type=int, help="The number of concurrent pubmed requests to make", default=10)
     parser.add_argument('--concurrent-ss', type=int, help="The number of concurrent Semantic Scholar requests to make", default=50)
     parser.add_argument('--concurrent-dm', type=int, help="The number of concurrent Dynamed requests to make", default=10)
+    parser.add_argument('--process-ai', action='store_true', help="Process the AI on the results", default=True)
     parser.add_argument('-r', '--retries', type=int, default=3, help='Number of retries to make')
     parser.add_argument('-v', '--verbose', action='count', help='Enable verbose mode', default=0)
     
@@ -92,6 +118,11 @@ async def main():
 
     results = await search(search_keywords, args.concurrent_pm, args.concurrent_ss, args.concurrent_dm, args.retries)
 
+    processor = None
+
+    if args.process_ai:
+        processor = PDFProcessor()
+
     if args.output_file:
         async with aiofiles.open(args.output_file, mode='w') as f:
             for s in results:
@@ -102,6 +133,8 @@ async def main():
         for s in results:
             if args.with_pdf_only and not s['pdf_url']:
                 continue
+            if args.process_ai:
+                s = await process_ai(processor, s)
             print(json.dumps(s))
 
 if __name__ == '__main__':
