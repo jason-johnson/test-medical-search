330ed4f9c0cf4c1dbc13e83681d38d88669234ea
Jason Johnson <jasojohnson@microsoft.com>
2024-10-28 13:18:05 +0100

    rest of changes for AI deployment

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,5 +5,24 @@ variable "branch" {
 
 variable "location" {
   description = "default location to use if not specified"
-  default = "westeurope"  
+  default = "switzerlandnorth"  
+}
+
+variable "openai_embedding_models" {
+  description = "list of embedding models to deploy"
+  default = [{
+    name     = "text-embedding-ada-002"
+    version  = "2"
+    capacity = 200
+    }, {
+    name    = "gpt-4"
+    version = "turbo-2024-04-09"
+    capacity = 150
+  }]
+  type = list(object({
+    name     = string
+    version  = optional(string, "1")
+    capacity = optional(number, 1)
+  }))
+
 }
